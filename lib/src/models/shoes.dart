01c84ec8949fc9fc23e2e29d2fa898471466d341
ihsuncloud <ihsun@tuta.io>
2024-08-26 07:01:11 +0330

    🚀 Add shoes model.

---
+++ lib/src/models/shoes.dart
@@ -0,0 +1,14 @@
+class Shoes {
+  final String title, image, price, discount;
+  final List<String> colors;
+  final List<String> sizes;
+
+  Shoes({
+    required this.title,
+    required this.image,
+    required this.price,
+    required this.discount,
+    required this.colors,
+    required this.sizes,
+  });
+}
