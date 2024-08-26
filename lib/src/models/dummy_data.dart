37ca94da87f9062486943a0fdc8158a24e6cc693
ihsuncloud <ihsun@tuta.io>
2024-08-26 07:01:23 +0330

    🚀 Add dummy data.

---
+++ lib/src/models/dummy_data.dart
@@ -0,0 +1,12 @@
+import 'package:nike_shoes_app/src/models/shoes.dart';
+
+List<Shoes> shoesList = [
+  Shoes(
+    title: '',
+    image: '',
+    discount: '',
+    price: '',
+    colors: [],
+    sizes: [],
+  ),
+];
