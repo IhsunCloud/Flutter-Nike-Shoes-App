b6c7df90add21d833535c2d4cd0872e7fe108c0b
ihsuncloud <ihsun@tuta.io>
2024-08-26 08:57:38 +0330

    🚀 Add shoe model.

---
+++ lib/src/models/shoe.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class Shoe extends Equatable {
+  final int id;
+  final String title;
+  final String imageUrl;
+  final String price;
+  final bool isLiked;
+
+  const Shoe({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.price,
+    this.isLiked = false,
+  });
+
+  Shoe copyWith({bool? isLiked}) {
+    return Shoe(
+      id: id,
+      title: title,
+      imageUrl: imageUrl,
+      price: price,
+      isLiked: isLiked ?? this.isLiked,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, title, imageUrl, price, isLiked];
+}
