d6b7fdd00ba87b35ce061fd6d3f0bbee913f44f0
ihsuncloud <ihsun@tuta.io>
2024-08-26 08:58:21 +0330

    🚀 Add notifier.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
+// Third-party packages.
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 // Local project.
 import 'package:nike_shoes_app/src/kernel/painless.dart';
 
-void main() => runApp(const Kernel());
+void main() {
+  runApp(
+    const ProviderScope(
+      child: Kernel(),
+    ),
+  );
+}
 
 class Kernel extends StatelessWidget {
   const Kernel({super.key});
