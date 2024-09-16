eca438b9968d19f79e495cab228cc4fd747c526a
ihsuncloud <ihsun@tuta.io>
2024-09-17 01:05:59 +0330

    🚀 Add color palette.

--- lib/src/kernel/constants/colors.dart
+++ lib/src/kernel/constants/colors.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/material.dart';
+
 class ColorPalette {
   // FLUSH MAHOGANY
-  static const String flushMahogany = '0xFFCD2626';
+  static const Color flushMahogany = Color(0xffCD2626);
   // LITTLE BLUE HERON
-  static const String littleBlueHeron = '0xFF394376';
+  static const Color littleBlueHeron = Color(0xff394376);
+  // LAKE LUCERNE
+  static const Color lakeLucerne = Color(0xff669bbc);
+  // HEARTFELT
+  static const Color heartFelt = Color(0xffffafcc);
 }
